1d0d3e897d29ef1fdddfb08b34da8e6a86b81c35
abdulquayumhp <badulquayumhp998@gmailc.com>
2022-11-25 01:19:17 +0600

    report data add

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const port = process.env.port || 8000;
 // mongodb user
 const uri = `mongodb+srv://${process.env.DB_ADMIN_USER}:${process.env.DB_PASSWORD}@cluster0.wtn02jv.mongodb.net/?retryWrites=true&w=majority`;
 
-console.log(uri);
+// console.log(uri);
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -24,15 +24,25 @@ const client = new MongoClient(uri, {
 
 async function mongodbConnect() {
   try {
+    // all product
     const furnitureResellingProduct = client
       .db("furnitureResell")
       .collection("furnitureResellProduct");
+
+    // all category
     const furnitureResellingCategory = client
       .db("furnitureResell")
       .collection("ResellCategory");
+
+    // all booking
     const furnitureResellingBooking = client
       .db("furnitureResell")
       .collection("ResellBooking");
+
+    // all report
+    const furnitureResellingReport = client
+      .db("furnitureResell")
+      .collection("ResellReport");
 
     // const user = { name: "abudl" };
 
@@ -48,9 +58,10 @@ async function mongodbConnect() {
     });
 
     // all furniture  data
-    app.get("/home/category/:id", async (req, res) => {
-      const id = req.params.id;
+    app.get("/home/category/:category", async (req, res) => {
+      const id = req.params.category;
       const query = { category: id };
+      console.log(query);
       const resellerAllCategory = await furnitureResellingProduct
         .find(query)
         .toArray();
@@ -58,6 +69,7 @@ async function mongodbConnect() {
     });
 
     // all booking fatch data
+    // booking
     app.post("/modalData", async (req, res) => {
       const modalData = req.body;
       // console.log("helo", modalData);
@@ -65,7 +77,15 @@ async function mongodbConnect() {
       //   email: modalData.user,
       // };
       // console.log("email", query);
-      const result = await furnitureResellingBooking.insertOne(modalData);
+      const option = await furnitureResellingBooking.insertOne(modalData);
+      // console.log(option);
+      res.send(option);
+    });
+    // report
+    app.post("/ReportData", async (req, res) => {
+      const ReportData = req.body;
+      const result = await furnitureResellingReport.insertOne(ReportData);
+      console.log(result);
       res.send(result);
     });
   } finally {
